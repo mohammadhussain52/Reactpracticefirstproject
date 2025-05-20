62a62fe7e2dd72a56f69224f262d53fb73d1f312
mohammadhussain52 <mhussain8440@gmail.com>
2025-05-20 18:32:08 +0500

    commit

--- src/pages/SinglejobPage.jsx
+++ src/pages/SinglejobPage.jsx
@@ -1,11 +1,37 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import Spinner from '../components/Spinner'
 import React from 'react'
 
 const SinglejobPage = () => {
-  return (
-    <div>
-      SingleJob page
-    </div>
-  )
+    const {id} = useParams()
+    const [job, setjob] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+  useEffect (() =>{
+    const fetchjob = async () =>{
+        try {
+            const res = await fetch(`http://localhost:8000/jobs/${id}`);
+            const data = await res.json();
+            console.log(data)
+            setjob(data);
+          }
+          catch (error) {
+            console.log('Error fetching data, error')   
+          }
+          finally {
+            setLoading(false)
+          }
+    }
+    fetchjob();
+
+  }, [])
+    return (
+    loading ? <Spinner /> : (
+    <h1>{job.title}</h1>)
+  );
 }
+
+// const jobloader = async ({})
 
 export default SinglejobPage
