ccc83148319fc93fba2b982ac6d6897e3bbabcc4
mohammadhussain52 <mhussain8440@gmail.com>
2025-05-22 11:04:36 +0500

    init

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,18 @@ import Homepage from './pages/Homepage';
 import Mainlayout from './layout/Mainlayout';
 import Jobspage from './pages/Jobspage';
 import NotFoundpage from './pages/NotFoundpage';
-import SinglejobPage from './pages/SinglejobPage';
+import SinglejobPage, {jobLoader} from './pages/SinglejobPage';
+import Addjobpage from './pages/Addjobpage';
+
+
 
 const router = createBrowserRouter(
   createRoutesFromElements (
   <Route path='/' element={<Mainlayout />}>
       <Route index element={<Homepage />} />
       <Route path='/jobs' element={<Jobspage />} />
-      <Route path='/job/:id' element={<SinglejobPage />} />
+      <Route path='/add-job' element={<Addjobpage />} />
+      <Route path='/job/:id' element={<SinglejobPage />} loader={jobLoader} />
       <Route path='/*' element={<NotFoundpage />} />
       </Route>
   )
