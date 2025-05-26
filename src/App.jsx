4f13be8ceb5aac1a08af43b7c548b08a5f20257d
mohammadhussain52 <mhussain8440@gmail.com>
2025-05-26 10:35:28 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,63 @@
-// src/App.jsx
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
-import React, { useEffect, useState } from "react";
-import './App.css';
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
+import React from "react";
+import './App.css'
 import Homepage from './pages/Homepage';
 import Mainlayout from './layout/Mainlayout';
 import Jobspage from './pages/Jobspage';
 import NotFoundpage from './pages/NotFoundpage';
-import SinglejobPage, { jobLoader } from './pages/SinglejobPage';
+import SinglejobPage, {jobLoader} from './pages/SinglejobPage';
 import Addjobpage from './pages/Addjobpage';
 import Editjobpage from './pages/Editjobpage';
-import { db } from './firebase';
-import { collection, addDoc, getDocs, deleteDoc, doc, updateDoc } from 'firebase/firestore';
+
+
+
 
 const App = () => {
-  const [jobs, setJobs] = useState([]);
-  const jobsCollection = collection(db, 'jobs');
+// Add New Job
+  const addjob =  async(newjob) => {
+    const res = await fetch('http://localhost:8000/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newjob)
+    });
+  }
 
-  // Fetch jobs from Firestore
-  const getJobs = async () => {
-    const snapshot = await getDocs(jobsCollection);
-    setJobs(snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })));
-  };
+  //Delete Job
+  const deletejob = async(id) => {
+    const res = await fetch(`http://localhost:8000/jobs/${id}`, {
+      method: 'DELETE'
+    });
+    return;
+  }
 
-  useEffect(() => {
-    getJobs();
-  }, []);
-
-  // Add new job
-  const addJob = async (job) => {
-    await addDoc(jobsCollection, job);
-    getJobs();
-  };
-
-  // Delete job
-  const deleteJob = async (id) => {
-    await deleteDoc(doc(db, 'jobs', id));
-    getJobs();
-  };
-
-  // Update job
-  const updateJob = async (id, updatedJob) => {
-    const jobRef = doc(db, 'jobs', id);
-    await updateDoc(jobRef, updatedJob);
-    getJobs();
-  };
-
-  // Router setup
+  //Update Job
+  const updatejob = async(job) => {
+    const res = await fetch( `http://localhost:8000/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(job)
+    });
+  }
+  
   const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path='/' element={<Mainlayout />}>
+    createRoutesFromElements (
+    <Route path='/' element={<Mainlayout />}>
         <Route index element={<Homepage />} />
-        <Route path='/jobs' element={<Jobspage jobs={jobs} />} />
-        <Route path='/add-job' element={<Addjobpage addJobSubmit={addJob} />} />
-        <Route
-          path='/edit-job/:id'
-          element={<Editjobpage updateJobSubmit={updateJob} />}
-          loader={jobLoader}
-        />
-        <Route
-          path='/job/:id'
-          element={<SinglejobPage deleteJob={deleteJob} />}
-          loader={jobLoader}
-        />
+        <Route path='/jobs' element={<Jobspage />} />
+        <Route path='/add-job' element={<Addjobpage addjobsubmit={addjob} />} />
+        <Route path='/edit-job/:id' element={<Editjobpage updatejobsubmit={updatejob}/>} loader={jobLoader} />
+        <Route path='/job/:id' element={<SinglejobPage deletejob={deletejob} />} loader={jobLoader} />
         <Route path='/*' element={<NotFoundpage />} />
-      </Route>
+        </Route>
     )
   );
 
-  return <RouterProvider router={router} />;
+  return (
+    <RouterProvider router={router} />
+  );
 };
-
 export default App;
